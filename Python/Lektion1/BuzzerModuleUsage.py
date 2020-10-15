76ab1d9277d5a63bedc33d5cc45e11d6fb480fc8
fm85 <famu1985@gmail.com>
2020-10-15 09:09:41 +0200

    details: Buzzer.time.sleep(1)

--- Python/Lektion1/BuzzerModuleUsage.py
+++ Python/Lektion1/BuzzerModuleUsage.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 import BuzzerGpioOutputExample as Buzzer
-import time
 
 Buzzer.initGPIO()
 Buzzer.switchOn()
-time.sleep(1)
+Buzzer.time.sleep(1)
 Buzzer.switchOff()
 Buzzer.cleanupGPIO()
