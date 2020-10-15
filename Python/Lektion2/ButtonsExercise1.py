b5380ff1168d71575facab9af1805bddc8ba0afa
fm85 <famu1985@gmail.com>
2020-10-15 15:10:51 +0200

    ButtonsExercise1.py added

--- Python/Lektion2/ButtonsExercise1.py
+++ Python/Lektion2/ButtonsExercise1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-#  ButtonsGpioInputExample.py
+#  ButtonsExercise1.py
 #  
 #  Copyright (C) 2020 by GBS St. Gallen, Inc. All rights reserved.
 #  Released under the therms of the GNU General Public License version 3
@@ -10,11 +10,15 @@ import RPi.GPIO as GPIO
 
 BUZZER_GPIO_BOARD_PIN = 12
 BUTTON_UP_GPIO_BOARD_PIN = 37
+BUTTON_RIGHT_GPIO_BOARD_PIN = 35
+RELAY_GPIO_BOARD_PIN = 40
 
 def initGPIO():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(BUZZER_GPIO_BOARD_PIN, GPIO.OUT)
+    GPIO.setup(RELAY_GPIO_BOARD_PIN, GPIO.OUT)
     GPIO.setup(BUTTON_UP_GPIO_BOARD_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(BUTTON_RIGHT_GPIO_BOARD_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def switchBuzzerOn():
     GPIO.output(BUZZER_GPIO_BOARD_PIN, GPIO.HIGH)
@@ -22,17 +26,30 @@ def switchBuzzerOn():
 def switchBuzzerOff():
     GPIO.output(BUZZER_GPIO_BOARD_PIN, GPIO.LOW)
 
-def isButtonUpPressed():
-    if (GPIO.input(BUTTON_UP_GPIO_BOARD_PIN) == 0):
+def switchRelayOn():
+    GPIO.output(RELAY_GPIO_BOARD_PIN, GPIO.LOW)
+    
+def switchRelayOff():
+    GPIO.output(RELAY_GPIO_BOARD_PIN, GPIO.HIGH)
+
+def isButtonPressed(boardPinNumber):
+    if (GPIO.input(boardPinNumber) == 0):
         return True
     else:
         return False
+    
+def isButtonUpPressed():
+    return isButtonPressed(BUTTON_UP_GPIO_BOARD_PIN)
+
+def isButtonRightPressed():
+    return isButtonPressed(BUTTON_RIGHT_GPIO_BOARD_PIN)
 
 def cleanupGPIO():
     GPIO.cleanup()
 
 def main():
     print("Buzzer- Warnton mit grüner UP- Taste auslösen!")
+    print("Relais mit grüner RIGHT- Taste einschalten!")
     print("Zum Beenden CTRL+C drücken, um den Keyboard- Interrupt auszulösen")
     initGPIO()
     try:
@@ -40,7 +57,12 @@ def main():
             if(isButtonUpPressed()):
                 switchBuzzerOn()
             else:
-                switchBuzzerOff()   
+                switchBuzzerOff()
+            
+            if(isButtonRightPressed()):
+                switchRelayOn()
+            else:
+                switchRelayOff()
     except KeyboardInterrupt:
         print("Das Programm wird beendet.")
         cleanupGPIO()
