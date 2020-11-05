ddf004b6eb56ddfa24f78e25ac308cd114ec0e0f
fm85 <famu1985@gmail.com>
2020-11-05 17:05:50 +0100

    FsWebcam.py added

--- Python/Lektion3/FsWebcam.py
+++ Python/Lektion3/FsWebcam.py
@@ -1,36 +1,28 @@
 #!/usr/bin/env python3
 #
-#  ButtonsExercise1.py
+#  FsWebcam.py
 #  
 #  Copyright (C) 2020 by GBS St. Gallen, Inc. All rights reserved.
 #  Released under the therms of the GNU General Public License version 3
 #  or later.
 #
 import RPi.GPIO as GPIO
+import os
+from datetime import datetime
 
-BUZZER_GPIO_BOARD_PIN = 12
 BUTTON_UP_GPIO_BOARD_PIN = 37
-BUTTON_RIGHT_GPIO_BOARD_PIN = 35
-RELAY_GPIO_BOARD_PIN = 40
+
+def takeAPicture():
+    actualTime = datetime.now()
+    path = '/home/pi/Pictures/'
+    filename = 'Aufnahme_' + actualTime.strftime('%Y-%m-%d-%H-%M-%S') + '.jpg'
+    command = 'fswebcam -r 1280-1024 ' + path + filename
+    print(command)
+    os.system(command)
 
 def initGPIO():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(BUZZER_GPIO_BOARD_PIN, GPIO.OUT)
-    GPIO.setup(RELAY_GPIO_BOARD_PIN, GPIO.OUT)
     GPIO.setup(BUTTON_UP_GPIO_BOARD_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(BUTTON_RIGHT_GPIO_BOARD_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-def switchBuzzerOn():
-    GPIO.output(BUZZER_GPIO_BOARD_PIN, GPIO.HIGH)
-    
-def switchBuzzerOff():
-    GPIO.output(BUZZER_GPIO_BOARD_PIN, GPIO.LOW)
-
-def switchRelayOn():
-    GPIO.output(RELAY_GPIO_BOARD_PIN, GPIO.LOW)
-    
-def switchRelayOff():
-    GPIO.output(RELAY_GPIO_BOARD_PIN, GPIO.HIGH)
 
 def isButtonPressed(boardPinNumber):
     if (GPIO.input(boardPinNumber) == 0):
@@ -41,28 +33,16 @@ def isButtonPressed(boardPinNumber):
 def isButtonUpPressed():
     return isButtonPressed(BUTTON_UP_GPIO_BOARD_PIN)
 
-def isButtonRightPressed():
-    return isButtonPressed(BUTTON_RIGHT_GPIO_BOARD_PIN)
-
 def cleanupGPIO():
     GPIO.cleanup()
 
 def main():
-    print("Buzzer- Warnton mit grüner UP- Taste auslösen!")
-    print("Relais mit grüner RIGHT- Taste einschalten!")
-    print("Zum Beenden CTRL+C drücken, um den Keyboard- Interrupt auszulösen")
+    print("Foto aufnehmen mit grüner UP- Taste!")
     initGPIO()
     try:
         while True:
             if(isButtonUpPressed()):
-                switchBuzzerOn()
-            else:
-                switchBuzzerOff()
-            
-            if(isButtonRightPressed()):
-                switchRelayOn()
-            else:
-                switchRelayOff()
+                takeAPicture()
     except KeyboardInterrupt:
         print("Das Programm wird beendet.")
         cleanupGPIO()
