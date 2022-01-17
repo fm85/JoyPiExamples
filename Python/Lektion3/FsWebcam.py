03d28f66c17ce26b6b2044004a3b95a215e6a608
fm85 <famu1985@gmail.com>
2022-01-17 17:08:36 +0100

    os.system replaced with subprocess.run

--- Python/Lektion3/FsWebcam.py
+++ Python/Lektion3/FsWebcam.py
@@ -2,12 +2,13 @@
 #
 #  FsWebcam.py
 #  
-#  Copyright (C) 2020 by GBS St. Gallen, Inc. All rights reserved.
+#  Copyright (C) 2022 by GBS St. Gallen, Inc. All rights reserved.
 #  Released under the therms of the GNU General Public License version 3
 #  or later.
 #
 import RPi.GPIO as GPIO
-import os
+from subprocess import run
+import time
 from datetime import datetime
 
 BUTTON_UP_GPIO_BOARD_PIN = 37
@@ -16,22 +17,19 @@ def takeAPicture():
     actualTime = datetime.now()
     path = '/home/pi/Pictures/'
     filename = 'Aufnahme_' + actualTime.strftime('%Y-%m-%d-%H-%M-%S') + '.jpg'
-    command = 'fswebcam -r 1280-1024 ' + path + filename
-    print(command)
-    os.system(command)
-
+    run(['fswebcam','-r','1280-1024', path + filename], shell=False)
+    
 def initGPIO():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(BUTTON_UP_GPIO_BOARD_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(
+        BUTTON_UP_GPIO_BOARD_PIN, GPIO.FALLING, 
+        callback=isButtonUpPressed,
+        bouncetime = 3000 
+    )
 
-def isButtonPressed(boardPinNumber):
-    if (GPIO.input(boardPinNumber) == 0):
-        return True
-    else:
-        return False
-    
-def isButtonUpPressed():
-    return isButtonPressed(BUTTON_UP_GPIO_BOARD_PIN)
+def isButtonUpPressed(boardPinNumber):
+    takeAPicture()
 
 def cleanupGPIO():
     GPIO.cleanup()
@@ -41,8 +39,7 @@ def main():
     initGPIO()
     try:
         while True:
-            if(isButtonUpPressed()):
-                takeAPicture()
+            time.sleep(5)
     except KeyboardInterrupt:
         print("Das Programm wird beendet.")
         cleanupGPIO()
